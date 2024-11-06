b37664ee3d64c09fdb73634b23f0faf707646b86
evetabadmin <ap@productshare.shop>
2024-11-06 15:26:01 -0500

    updated cors issues

--- src/config/plugins/assets.ts
+++ src/config/plugins/assets.ts
@@ -1,34 +1,32 @@
-import {
-  AssetServerPlugin,
-  configureS3AssetStorage,
-} from "@vendure/asset-server-plugin";
-import { DefaultAssetNamingStrategy } from "@vendure/core";
+import {AssetServerPlugin, configureS3AssetStorage,} from "@vendure/asset-server-plugin";
+import {DefaultAssetNamingStrategy} from "@vendure/core";
 import path from "path";
-import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
-import { getEnvs } from "../../getEnvs";
+import {fromNodeProviderChain} from "@aws-sdk/credential-providers";
+import {getEnvs} from "../../getEnvs";
 
 const region = process.env.AWS_REGION || "us-east-2";
 const credentials = fromNodeProviderChain({
-  ...(process.env.AWS_PROFILE && { profile: process.env.AWS_PROFILE }),
-  clientConfig: { region },
+	...(process.env.AWS_PROFILE && {profile: process.env.AWS_PROFILE}),
+	clientConfig: {region},
 });
-const nativeS3Configuration = { region, credentials };
-const { S3_BUCKET: bucket } = getEnvs();
+const nativeS3Configuration = {region, credentials};
+const {S3_BUCKET: bucket} = getEnvs();
 
 export const AssetsPlugin = AssetServerPlugin.init({
-  route: "assets",
-  assetUploadDir: path.join(__dirname, "assets"),
-  namingStrategy: new DefaultAssetNamingStrategy(),
-  /* storageStrategyFactory: configureS3AssetStorage({
-    bucket,
-    credentials,
-    nativeS3Configuration,
-  }), */
-  storageStrategyFactory: configureS3AssetStorage({
-    bucket: 'productshare',
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
-    },
-  }),
+	route: "assets",
+	assetUploadDir: path.join(__dirname, "assets"),
+	namingStrategy: new DefaultAssetNamingStrategy(),
+	/* storageStrategyFactory: configureS3AssetStorage({
+	  bucket,
+	  credentials,
+	  nativeS3Configuration,
+	}), */
+	storageStrategyFactory: configureS3AssetStorage({
+		bucket: 'productshare',
+		credentials: {
+			accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+		},
+		nativeS3Configuration: {region: process.env.AWS_REGION,},
+	}),
 });
