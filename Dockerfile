a72f328e7cb21d4476156dcdf4a43f0ae4942d09
PSAnton <ap@productshare.shop>
2025-11-02 17:32:49 -0500

    downgraded Vendure to version 2.3, updated dependencies and Dockerfile. Added `NativeAuthenticationStrategy` to support native authentication.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM node:18
+FROM node:22
 
 WORKDIR /usr/src/app
 
 COPY package*.json ./
-RUN npm install --production
+RUN npm ci
 RUN npm install -g concurrently
 COPY . .
 RUN npm run build
+
+# Optionally remove dev dependencies after build to reduce image size
+RUN npm prune --production
--- src/config/auth_options/index.ts
+++ src/config/auth_options/index.ts
@@ -1,23 +1,22 @@
-import { VendureConfig } from "@vendure/core";
-import { getEnvs } from "../../getEnvs";
-import { PrivyAuthenticationStrategy } from "./privy-auth-strategy";
+import { VendureConfig } from '@vendure/core';
+import { getEnvs } from '../../getEnvs';
+import { PrivyAuthenticationStrategy } from './privy-auth-strategy';
+import { NativeAuthenticationStrategy } from '@vendure/core';
 
 const { SUPERADMIN_PASSWORD, SUPERADMIN_USERNAME, COOKIE_SECRET } = getEnvs();
 
-export const authOptions: VendureConfig["authOptions"] = {
-  tokenMethod: ["bearer", "cookie"],
-  superadminCredentials: {
-    identifier: SUPERADMIN_USERNAME,
-    password: SUPERADMIN_PASSWORD,
-  },
-  cookieOptions: {
-    secret: COOKIE_SECRET,
-    ...(process.env.APP_ENV === "dev"
-      ? {}
-      : { domain: ".productshare.net", sameSite: "lax"}),
-  
-  },
-  shopAuthenticationStrategy: [
-    new PrivyAuthenticationStrategy(process.env.PRIVY_APP_ID, process.env.PRIVY_APP_SECRET),
-  ],
+export const authOptions: VendureConfig['authOptions'] = {
+    tokenMethod: ['bearer', 'cookie'],
+    superadminCredentials: {
+        identifier: SUPERADMIN_USERNAME,
+        password: SUPERADMIN_PASSWORD,
+    },
+    cookieOptions: {
+        secret: COOKIE_SECRET,
+        ...(process.env.APP_ENV === 'dev' ? {} : { domain: '.productshare.net', sameSite: 'lax' }),
+    },
+    shopAuthenticationStrategy: [
+        new PrivyAuthenticationStrategy(process.env.PRIVY_APP_ID, process.env.PRIVY_APP_SECRET),
+        new NativeAuthenticationStrategy(),
+    ],
 };
